7ef1d4eb8806b3f1feece6dd24fa48c6a1f74b56
dusksama <mewjw@hotmail.com>
2016-06-04 05:25:16 +0800

    to display tasks

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Task
 
 def tasknote_list(request):
-    return render(request, 'myapp/tasknote_list.html', {})
+    tasks = Task.objects.filter(due_date__lte=timezone.now()).order_by('due_date')
+    return render(request, 'myapp/tasknote_list.html', {'tasks': tasks})
