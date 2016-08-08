2f4285566dc43b38163d513852246d80c944ac6f
dusksama <mewjw@hotmail.com>
2016-08-08 17:58:53 +0800

    update

--- myapp/views.py
+++ myapp/views.py
@@ -6,6 +6,8 @@ from .choices import *
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 
+from django.core import serializers
+
 def tasknote_list(request):
     tasks = Task.objects.order_by('due_date')
     notes = Note.objects.filter(created_date__lte=timezone.now())
@@ -41,6 +43,18 @@ def note_new(request):
     if request.method == "POST":
         form = NoteForm(request.POST)
         if form.is_valid():
+            note = Note()
+            note.subject = form.cleaned_data['subject']
+            note.text_area = form.cleaned_data['text_area']
+            note.visibility_status = form.cleaned_data['visibility_status']
+            note.assigned_to = form.cleaned_data['assigned_to']
+            note.author = User.objects.get(id=1)
+            note.created_date = timezone.now()
+            JSONSerializer = serializers.get_serializer("json")
+            json_serializer = JSONSerializer()
+            print (request.POST.as_json())
+            with open("file.json", "w") as out:
+                json_serializer.serialize(Note.objects.all(), stream=out)
             return redirect('tasknote_list')
     else:
         form = NoteForm()
